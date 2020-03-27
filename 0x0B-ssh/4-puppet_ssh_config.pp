ca793f29f49903a6d14dbdc40a6d840c9a878395
anaruzz <laabidigh@gmail.com>
2020-03-27 20:43:52 +0100

    Adv Task

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,5 +3,4 @@ file_line {'~/.ssh/config':
 ensure  => 'present',
 path    => '~/.ssh/config',
 mode    => '0744',
-  content => 'Host 35.237.161.240\nUser ubuntu\IdentityFile ~/.ssh/holberton\nPasswordAuthentication no'
-}
+content => 'Host 35.237.161.240\nUser ubuntu\IdentityFile ~/.ssh/holberton\nPasswordAuthentication no'
