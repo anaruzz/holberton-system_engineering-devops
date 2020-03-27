2a11505392091015f698f0ce372392d3bd453721
anaruzz <laabidigh@gmail.com>
2020-03-27 20:40:48 +0100

    Adv Task

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Changes SSH config file
 file_line { ~/.ssh/holberton:
   ensure            => present,
-  path              => '/etc/ssh/sshd_config',
+  path              => '~/.ssh/config',
   match_for_absence => true,
   mode    => '0744',
   content => 'Host 35.237.161.240\nUser ubuntu\IdentityFile ~/.ssh/holberton\nPasswordAuthentication no'
