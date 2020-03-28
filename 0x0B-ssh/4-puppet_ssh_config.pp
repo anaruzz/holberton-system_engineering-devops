88a0be069c5d868c026664fa80ffa84e2e95c393
anaruzz <laabidigh@gmail.com>
2020-03-28 21:49:24 +0100

    Advanced Task Done

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 file {'~/.ssh/holberton':
 ensure  => 'present',
 path    => '/etc/ssh/ssh_config',
-content => 'Host 35.237.161.240\nUser ubuntu\IdentityFile ~/.ssh/holberton\nPasswordAuthentication no',
+content => 'PasswordAuthentication no\nIdentityFile ~/.ssh/holberton',
 }
