3182d70e621432d140efaa6dd2da85a5d53c390f
anaruzz <laabidigh@gmail.com>
2020-04-07 04:34:38 +0100

    Adv fix

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -11,5 +11,5 @@ ensure  => running,
 
 exec {'configure and restart':
 provider => shell,
-command  => 'host=$(hostname);nl="\\\tadd_header X-Served-By \"$host\";";sudo sed -i -e "/sendfile/i $nl" /etc/nginx/nginx.conf;sudo service nginx restart',
+command  => 'sed -i -e "/sendfile/i \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart',
 }
