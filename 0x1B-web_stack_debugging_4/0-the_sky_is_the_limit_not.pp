8566ddc48a6acd989a4f1bc3f2b20668f4dbd7ef
anaruzz <laabidigh@gmail.com>
2020-07-02 21:52:48 +0100

    try

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,13 @@
 # increase number of limited open files in a server
 exec { 'change_number':
-  command =>'sed -i \'s/15/5000/g\' etc/default/nginx',
-  path    =>['/bin'],
+  command =>'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/g" /etc/default/nginx',
+  path    =>['/usr/bin', '/bin'],
 }
 exec {'reload_nginx':
-  command =>'service nginx restart',
-  path    =>['/bin'],
+  command =>'sudo service nginx reload',
+  path    =>['/usr/bin', '/bin'],
+}
+exec {'restart_nginx':
+  command =>'sudo service nginx restart',
+  path    =>['/usr/bin', '/bin'],
 }
