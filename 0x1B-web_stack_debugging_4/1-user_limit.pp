3694128cf84c2f4bd3133ae4bc366337b6794696
anaruzz <laabidigh@gmail.com>
2020-07-03 04:47:40 +0100

    adv

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Change OS configuration to allow login with holberton user
+
+exec {'holberton hard':
+command => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 65534/g' /etc/security/limits.conf; /sbin/sysctl -p",
+path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+}
+exec {'holberton soft':
+command => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 65534/g' /etc/security/limits.conf; /sbin/sysctl -p",
+path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+}
